734c3db9e9f923014a5a879a8e5fd06ef3ac56cf
root <root@tr.localdomain>
2017-04-19 16:54:28 +0000

    recreate fix issue

--- recreate_lxc.sh
+++ recreate_lxc.sh
@@ -1,9 +1,10 @@
 lxc stop system
 lxc delete system 
-lxc lauch ubuntu16 system
+lxc launch ubuntu16 system
 lxc stop system 
 lxc network attach systemBr system default eth0
-lxc start system
+
+lxc restart system
 sleep 5 
 echo "Done"
 
@@ -14,9 +15,9 @@ sys_ip=$(lxc info sys | grep -m1 "eth0..inet" | cut -f 3)
 lxc exec system -- sed -i '/PasswordAuthentication no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
 lxc exec system -- sed -i '/PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config > /dev/null
 
-/root/HoneySSH/setup_ssh_redirection_honeypot.sh system $sys_ip
+#/root/HoneySSH/setup_ssh_redirection_honeypot.sh system $sys_ip
 
-sed -i "/honey_ip =/c\honey_ip = $ip" honssh.cfg
+#sed -i "/honey_ip =/c\honey_ip = $ip" honssh.cfg
 /root/HoneySSH/honsshctrl.sh stop
 /root/HoneySSH/honsshctrl.sh clean 
 /root/HoneySSH/honsshctrl.sh start
