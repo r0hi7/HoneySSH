a19aa47caf046acd867840dcf01e1cf3cf13baff
root <root@sf.localdomain>
2017-04-19 11:01:17 +0000

    Crecreting sys script

---
+++ recreate_lxc.sh
@@ -0,0 +1,20 @@
+lxc stop system
+lxc copy system system2
+lxc delete system 
+lxc copy system2 system
+lxc attach network systemBr system default eth0
+ip=$(lxc info system | grep -m1 "eth0..inet" | cut -f 3)
+sys_ip=$(lxc info sys | grep -m1 "eth0..inet" | cut -f 3)
+
+
+lxc exec system -- sed -i '/PasswordAuthentication no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
+lxc exec system -- sed -i '/PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config > /dev/null
+
+/root/HoneySSH/setup_ssh_redirection_honeypot.sh system $sys_ip
+
+sed -i "/honey_ip =/c\honey_ip = $ip" honssh.cfg
+/root/HoneySSH/honsshctrl.sh stop
+/root/HoneySSH/honsshctrl.sh clean 
+/root/HoneySSH/honsshctrl.sh start
+
+
